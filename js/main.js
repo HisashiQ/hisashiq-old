ce6677e926da0e9445c2b1333567c7afe81a21c4
HisashiQ <quinnkoike@gmail.com>
2017-06-08 19:46:31 +0100

    Colour change, transations added, links changed

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@ $( document ).ready(function() {
   setTimeout(
   function()
   {
+    Materialize.showStaggeredList('#list-main');
+    $('#list-parent').removeClass('hide');
+  }, 300);
+  setTimeout(
+  function()
+  {
     $('.tap-target').tapTarget('open');
   }, 4000);
     $('#image-me').removeClass("scale-out").addClass("scale-in");
